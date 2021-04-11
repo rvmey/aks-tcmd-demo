435fe2a63f94ee20381c7d5b0649f2077581f586
Russell VanderMey <russell@vandermey.us>
2021-04-10 21:49:31 -0400

    Improvements

--- demo.sh
+++ demo.sh
@@ -1,19 +1,21 @@
 # Create a Azure Kubernetes cluster
-az group create --location eastus --resource-group russ-aks
-az aks create --resource-group russ-aks --name demo-cluster --node-count 1
-az aks get-credentials --name demo-cluster --resource-group russ-aks
+# az group create --location eastus --resource-group russ-aks
+# az aks create --resource-group russ-aks --name demo-cluster --node-count 1
+# az aks get-credentials --name demo-cluster --resource-group russ-aks
 
 # Get your TRIGGERcmd token
 export TOKEN=$(cat ~/.TRIGGERcmdData/token.tkn)
 
 # Create a computer object in the TRIGGERcmd account for the container running in Kubernetes
-export AKS_TCMD_COMPUTER="AKS_DEMO_CONTAINER"
-export COMPUTER_ID=$(curl -H "Authorization: Bearer ${TOKEN}" https://www.triggercmd.com/api/computer/save?name=${AKS_TCMD_COMPUTER} | jq -r .data.id)
+export COMPUTER_NAME="AKS_Demo_Container"
+export COMPUTER_ID=$(curl -H "Authorization: Bearer ${TOKEN}" "https://www.triggercmd.com/api/computer/save?name=${COMPUTER_NAME}&voice=container" | jq -r .data.id)
 
 # Create a secret containing the token and the computer ID.  
+kubectl delete secret generic tcmd-secret > /dev/null 2>&1
 kubectl create secret generic tcmd-secret --from-literal=token=${TOKEN} --from-literal=computerid=${COMPUTER_ID}
 
 # Create a configmap with the commands you can run on the container via TRIGGERcmd
+kubectl delete configmap commands-config > /dev/null 2>&1
 kubectl create configmap commands-config --from-literal=commands='
 [
     {
@@ -23,12 +25,14 @@ kubectl create configmap commands-config --from-literal=commands='
     }, 
     {
         "trigger": "Run nginx",
-        "command": "/kubectl run nginx --image=nginx",
-        "offCommand": "/kubectl delete po nginx",
+        "command": "kubectl run nginx --image=nginx",
+        "offCommand": "kubectl delete po nginx",
         "ground": "foreground",
-        "voice": "engine x"
+        "voice": "engine x",
+        "allowParams": "true"
     }
 ]'
 
 # Create the tcmd deployment
+kubectl delete deploy tcmd-deployment > /dev/null 2>&1
 kubectl apply -f ./kubernetes_manifests
--- tcmd_container_scripts/prereqs.sh
+++ tcmd_container_scripts/prereqs.sh
@@ -1,3 +1,4 @@
 apt-get update -y && apt install curl -y
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
+mv kubectl /usr/local/bin
